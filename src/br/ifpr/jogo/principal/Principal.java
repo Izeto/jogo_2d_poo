89d8ed530987e267286f0f5fe9dbda8e2d47d46d
José Ciriaco <joseantoniociri@gmail.com>
2023-05-23 22:50:33 -0300

    Criação do Projeto

---
+++ src/br/ifpr/jogo/principal/Principal.java
@@ -0,0 +1,18 @@
+package br.ifpr.jogo.principal;
+
+import javax.swing.JFrame;
+
+public class Principal extends JFrame {
+
+    public Principal(){
+        super.setVisible(true);
+        this.setSize(500, 500);
+        this.setTitle("Meu Jogo");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        Principal principal = new Principal();
+    }
+}
